64de0bb2c8cddb84c5e85e2e486cb6e46c3f6950
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-11 12:54:22 +0900

    Finish ch7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password,
+      :password_confitmation
+    )
   end
 end
