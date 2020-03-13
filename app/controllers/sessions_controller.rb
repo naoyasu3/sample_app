a795ee78f1d965f9f371de1ef0d96985fbf1a915
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-13 11:52:17 +0900

    ch10 finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,20 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # Success
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
+      # Failure
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
+
   def destroy
     log_out if logged_in?
-    redirect_to root_path
+    redirect_to root_url
   end
 end
