6ad3440be77f2713e63296d35789db7723e066a7
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-17 16:04:11 +0900

    user_signup rspec test GREEN

---
+++ spec/requests/users_signup_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+RSpec.describe "Users signup", type: :request do
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  example "valid signup information with account activation" do
+    get signup_path
+    expect{
+      post users_path, params: { user: { name:  "Example User",
+        email: "user@example.com",
+        password:              "password",
+        password_confirmation: "password" } }
+    }.to change(User, :count).by(1)
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    expect(user.activated?).to be false
+    # 有効化していない状態でログインしてみる
+    log_in_as(user)
+    expect(is_logged_in?).to be true
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    expect(is_logged_in?).to be false
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    expect(is_logged_in?).to be false
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+end
