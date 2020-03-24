b00c7a272446268d76a88d683d8105aa602f2bb3
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-24 17:14:54 +0900

    validates green

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :room_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :following, through: 'active_relationships', source: 'followed'
   has_many :followers, through: "passive_relationships", source: 'follower'
   has_many :comments, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
